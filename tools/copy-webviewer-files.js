c0f1eb4c1b10195b810cf0ad51f26fe69834fbaf
Armando DBG <bollain@gmail.com>
2020-04-13 17:01:22 -0700

    Fix issue in copy files script (#8)

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -1,12 +1,16 @@
 const fs = require('fs-extra');
+const path = require('path');
 
 const copyFiles = async () => {
+  const sourcePath = path.resolve('./node_modules/@pdftron/webviewer/public');
+  const destPath = path.resolve('./src/lib/');
+
   try {
-    await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
+    await fs.copy(sourcePath, destPath);
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
   }
 };
 
-copyFiles();å
+copyFiles();
