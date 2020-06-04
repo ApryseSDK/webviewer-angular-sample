d2da0eb558a4423b6e06729dc01e4981fdcf1c7b
Shaun Cruz <scruz@sertifi.com>
2020-06-04 13:20:09 -0500

    Added elements to demo bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('viewer', { static: false }) viewer: ElementRef;
   wvInstance: any;
 
+  colorChange = false;
+
   ngAfterViewInit(): void {
 
     WebViewer({
@@ -18,13 +20,24 @@ export class AppComponent implements OnInit, AfterViewInit {
     }, this.viewer.nativeElement).then(instance => {
       this.wvInstance = instance;
 
+      instance.setHeaderItems(header => {
+        header.push({
+          type: 'actionButton',
+          // tslint:disable-next-line: max-line-length
+          img: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="rocket" class="svg-inline--fa fa-rocket fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505.12019,19.09375c-1.18945-5.53125-6.65819-11-12.207-12.1875C460.716,0,435.507,0,410.40747,0,307.17523,0,245.26909,55.20312,199.05238,128H94.83772c-16.34763.01562-35.55658,11.875-42.88664,26.48438L2.51562,253.29688A28.4,28.4,0,0,0,0,264a24.00867,24.00867,0,0,0,24.00582,24H127.81618l-22.47457,22.46875c-11.36521,11.36133-12.99607,32.25781,0,45.25L156.24582,406.625c11.15623,11.1875,32.15619,13.15625,45.27726,0l22.47457-22.46875V488a24.00867,24.00867,0,0,0,24.00581,24,28.55934,28.55934,0,0,0,10.707-2.51562l98.72834-49.39063c14.62888-7.29687,26.50776-26.5,26.50776-42.85937V312.79688c72.59753-46.3125,128.03493-108.40626,128.03493-211.09376C512.07526,76.5,512.07526,51.29688,505.12019,19.09375ZM384.04033,168A40,40,0,1,1,424.05,128,40.02322,40.02322,0,0,1,384.04033,168Z"></path></svg>',
+          onClick: () => {
+            this.toggleColorChange();
+          }
+        });
+      });
+
       // now you can access APIs through this.webviewer.getInstance()
       instance.openElements(['notesPanel']);
       // see https://www.pdftron.com/documentation/web/guides/ui/apis for the full list of APIs
 
       // or listen to events from the viewer element
       this.viewer.nativeElement.addEventListener('pageChanged', (e) => {
-        const [ pageNumber ] = e.detail;
+        const [pageNumber] = e.detail;
         console.log(`Current page is ${pageNumber}`);
       });
 
@@ -33,8 +46,8 @@ export class AppComponent implements OnInit, AfterViewInit {
         console.log('annotations loaded');
       });
 
-      instance.docViewer.on('documentLoaded', this.wvDocumentLoadedHandler)
-    })
+      instance.docViewer.on('documentLoaded', this.wvDocumentLoadedHandler);
+    });
   }
 
   ngOnInit() {
@@ -58,5 +71,11 @@ export class AppComponent implements OnInit, AfterViewInit {
     annotManager.addAnnotation(rectangle);
     annotManager.drawAnnotations(rectangle.PageNumber);
     // see https://www.pdftron.com/api/web/WebViewer.html for the full list of low-level APIs
+  }
+
+  toggleColorChange() {
+    console.log('log');
+
+    this.colorChange = !this.colorChange;
   }
 }
