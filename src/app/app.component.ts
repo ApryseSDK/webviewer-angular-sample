c9f5ff7872896f6303f0b2e8e509f3f82b63feee
Will Wang <wang.yw.william@gmail.com>
2021-11-23 15:55:56 -0800

    poc of angular with video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit, Output, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
+import { initializeVideoViewer } from '@pdftron/webviewer-video';
 
 @Component({
   selector: 'app-root',
@@ -22,11 +23,28 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     WebViewer({
       path: '../lib',
-      initialDoc: '../files/webviewer-demo-annotated.pdf'
-    }, this.viewer.nativeElement).then(instance => {
+    }, this.viewer.nativeElement).then(async instance => {
+      const license = `---- Insert commercial license key here after purchase ----`;
+      const videoUrl = 'https://pdftron.s3.amazonaws.com/downloads/pl/video/video.mp4';
+
       this.wvInstance = instance;
 
-      this.coreControlsEvent.emit(instance.UI.LayoutMode.Single);
+      //this.coreControlsEvent.emit(instance.UI.LayoutMode.Single);
+
+      const videoInstance = await initializeVideoViewer(
+        //@ts-ignore
+        instance,
+        {
+          license,
+        }
+      );
+
+      // Load a video at a specific url. Can be a local or public link
+      // If local it needs to be relative to lib/ui/index.html.
+      // Or at the root. (eg '/video.mp4')
+      videoInstance.loadVideo(videoUrl);
+
+
 
       const { documentViewer, Annotations, annotationManager } = instance.Core;
 
@@ -36,20 +54,20 @@ export class AppComponent implements OnInit, AfterViewInit {
         console.log('annotations loaded');
       });
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        this.documentLoaded$.next();
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-        annotationManager.addAnnotation(rectangleAnnot);
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+      // documentViewer.addEventListener('documentLoaded', () => {
+      //   this.documentLoaded$.next();
+      //   const rectangleAnnot = new Annotations.RectangleAnnotation({
+      //     PageNumber: 1,
+      //     // values are in page coordinates with (0, 0) in the top left
+      //     X: 100,
+      //     Y: 150,
+      //     Width: 200,
+      //     Height: 50,
+      //     Author: annotationManager.getCurrentUser()
+      //   });
+      //   annotationManager.addAnnotation(rectangleAnnot);
+      //   annotationManager.redrawAnnotation(rectangleAnnot);
+      // });
     })
   }
 
