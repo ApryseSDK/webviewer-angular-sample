168547796bc62021039a8d6fb88599fd69dfa6b0
Kristian Hein <kristianhein@gmail.com>
2021-12-09 07:57:23 -0800

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,6 @@ export class AppComponent implements OnInit, AfterViewInit {
 
       this.wvInstance = instance;
 
-      //this.coreControlsEvent.emit(instance.UI.LayoutMode.Single);
-
       const videoInstance = await initializeVideoViewer(
         //@ts-ignore
         instance,
@@ -43,31 +41,6 @@ export class AppComponent implements OnInit, AfterViewInit {
       // If local it needs to be relative to lib/ui/index.html.
       // Or at the root. (eg '/video.mp4')
       videoInstance.loadVideo(videoUrl);
-
-
-
-      const { documentViewer, Annotations, annotationManager } = instance.Core;
-
-      instance.UI.openElements(['notesPanel']);
-
-      documentViewer.addEventListener('annotationsLoaded', () => {
-        console.log('annotations loaded');
-      });
-
-      // documentViewer.addEventListener('documentLoaded', () => {
-      //   this.documentLoaded$.next();
-      //   const rectangleAnnot = new Annotations.RectangleAnnotation({
-      //     PageNumber: 1,
-      //     // values are in page coordinates with (0, 0) in the top left
-      //     X: 100,
-      //     Y: 150,
-      //     Width: 200,
-      //     Height: 50,
-      //     Author: annotationManager.getCurrentUser()
-      //   });
-      //   annotationManager.addAnnotation(rectangleAnnot);
-      //   annotationManager.redrawAnnotation(rectangleAnnot);
-      // });
     })
   }
 
